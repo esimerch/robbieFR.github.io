7546a5be56ce18b7f9b6caabbb5c115e9cbdc18c
RobbieFR <48907292+RobbieFR@users.noreply.github.com>
2023-11-28 22:21:01 +0100

    Add files via upload

---
+++ quizz/script.js
@@ -0,0 +1,14 @@
+function check() {
+    if (1=1){
+        alert("oui");
+    }  
+    else{
+        alert("non");
+    
+    };
+  }
+
+  function next() {
+    alert("next question please bro");
+  }
+
