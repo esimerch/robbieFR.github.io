6a57c98812050bde35fbaaad04ea3a09a6f23099
RobbieFR <48907292+RobbieFR@users.noreply.github.com>
2023-12-09 00:00:15 +0100

    Upload v0.0.1

--- quizz/script.js
+++ quizz/script.js
@@ -1,14 +1,113 @@
-function check() {
-    if (1=1){
-        alert("oui");
-    }  
-    else{
-        alert("non");
-    
-    };
-  }
 
-  function next() {
-    alert("next question please bro");
+
+let currentQuestionIndex = 0;
+let score = 0;
+let questions;
+
+// Load questions from the JSON file
+fetch('questions.json')
+  .then(response => response.json())
+  .then(data => {
+    questions = data;
+    renderMathInElement(document.body);
+    startQuizz(); // Call startQuizz only when questions are loaded
+  })
+  .catch(error => console.error('Error loading questions:', error));
+
+
+
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+function renderMath() {
+  MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
+}
+
+function startQuizz(){
+  currentQuestionIndex =0;
+  score=0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion(){
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+  questionElement.innerHTML = questionNo + ". "+ currentQuestion.question;
+  currentQuestion.answers.forEach(answer => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn")
+    answerButtons.appendChild(button);
+    renderMathInElement(document.body);
+    if(answer.correct){
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  })
+}
+
+
+function resetState(){
+  nextButton.style.display = "none";
+  while(answerButtons.firstChild){
+    answerButtons.removeChild(answerButtons.firstChild);
   }
+}
+
+function selectAnswer(e){
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if(isCorrect){
+    selectedBtn.classList.add("correct");
+    score++;
+  }else{
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach(button => {
+    if(button.dataset.correct === "true"){
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function showScore(){
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length}`;
+  nextButton.innerHTML = "Play again";
+  nextButton.style.display = "block";
+}
+
+function handleNextButton(){
+  currentQuestionIndex++;
+  if(currentQuestionIndex < questions.length){
+    showQuestion();
+  }else{
+    showScore();
+  }
+}
+
+
+nextButton.addEventListener("click", ()=>{
+  if(currentQuestionIndex < questions.length){
+    handleNextButton();
+  }else{
+    startQuizz();
+  }
+});
+
+src="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js"
+function latex(){
+  document.addEventListener("DOMContentLoaded", function() {
+    var tex = document.getElementsByClassName("btn");
+    for (var i = 0; i < tex.length; i++) {
+      katex.render(tex[i].innerHTML, tex[i]);
+    }
+});
+}
 
