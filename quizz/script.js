a6dcf8ba39ab3a2a91055fa950906b349cff0e49
RobbieFR <48907292+RobbieFR@users.noreply.github.com>
2023-12-10 09:53:58 +0100

    First real preview
    To fix : 
     - click on button

--- quizz/script.js
+++ quizz/script.js
@@ -9,10 +9,14 @@ fetch('questions.json')
   .then(response => response.json())
   .then(data => {
     questions = data;
+    console.log(questions);
+    //console.log(questions.length);  //1 
+    //console.log(questions[0].content[0].answers.length);
+    console.log("Nb de questions : " + questions[0].content.length);
     renderMathInElement(document.body);
     startQuizz(); // Call startQuizz only when questions are loaded
   })
-  .catch(error => console.error('Error loading questions:', error));
+  .catch(error => console.error("Error loading questions:", error));
 
 
 
@@ -20,23 +24,26 @@ fetch('questions.json')
 const questionElement = document.getElementById("question");
 const answerButtons = document.getElementById("answer-buttons");
 const nextButton = document.getElementById("next-btn");
+const skipButton = document.getElementById("skip-btn");
 
 function renderMath() {
   MathJax.Hub.Queue(["Typeset", MathJax.Hub]);
 }
 
 function startQuizz(){
-  currentQuestionIndex =0;
-  score=0;
+  currentQuestionIndex = 0;
+  score = 0;
   nextButton.innerHTML = "Next";
-  showQuestion();
+  showQuestion();  
 }
-
+//[0].content[0].answers[5]
 function showQuestion(){
   resetState();
-  let currentQuestion = questions[currentQuestionIndex];
+  let currentQuestion = questions[0].content[currentQuestionIndex];
   let questionNo = currentQuestionIndex + 1;
-  questionElement.innerHTML = questionNo + ". "+ currentQuestion.question;
+  //console.log(currentQuestion.answers[1]);
+  questionElement.innerHTML = "Question n°" + questionNo + ": "+ currentQuestion.question;
+  console.log(currentQuestion.answers);
   currentQuestion.answers.forEach(answer => {
     const button = document.createElement("button");
     button.innerHTML = answer.text;
@@ -78,14 +85,15 @@ function selectAnswer(e){
 
 function showScore(){
   resetState();
-  questionElement.innerHTML = `You scored ${score} out of ${questions.length}`;
+  questionElement.innerHTML = `Votre note : $$ ${score} / ${questions[0].content.length} $$`;
+  renderMathInElement(document.body);
   nextButton.innerHTML = "Play again";
   nextButton.style.display = "block";
 }
 
 function handleNextButton(){
   currentQuestionIndex++;
-  if(currentQuestionIndex < questions.length){
+  if(currentQuestionIndex < questions[0].content.length){
     showQuestion();
   }else{
     showScore();
@@ -94,14 +102,29 @@ function handleNextButton(){
 
 
 nextButton.addEventListener("click", ()=>{
-  if(currentQuestionIndex < questions.length){
+  if(currentQuestionIndex < questions[0].content.length){
     handleNextButton();
   }else{
     startQuizz();
   }
 });
 
-src="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js"
+
+function isNumber(value) {
+  return typeof value === 'number';
+}
+
+skipButton.addEventListener("click", ()=>{                                       //debug
+  var questionDemandee = prompt("A quelle question voulez-vous aller ? (UN CHIFFRE SINON CA CRASH)");
+  questionDemandee = parseInt(questionDemandee);  //str to int
+  console.log(isNumber(questionDemandee));
+  if( isNumber(questionDemandee) ){
+    currentQuestionIndex = questionDemandee -1 ;  // -1 for the index (ie : question 4 is index 3)
+    showQuestion();
+  }
+});
+
+src="https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js"    //func for rendering maths with katex
 function latex(){
   document.addEventListener("DOMContentLoaded", function() {
     var tex = document.getElementsByClassName("btn");
